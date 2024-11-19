4ec2c7f6bb57edcf43576433abb9b652f65533ee
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2024-11-20 01:15:54 +0800

    interface

--- x/customauth/types/expected_keepers.go
+++ x/customauth/types/expected_keepers.go
@@ -10,6 +10,7 @@ import (
 type AccountKeeper interface {
 	GetAccount(context.Context, sdk.AccAddress) sdk.AccountI // only used for simulation
 	// Methods imported from account should be defined here
+	GetModuleAddress(moduleName string) sdk.AccAddress
 }
 
 // BankKeeper defines the expected interface for the Bank module.
