c81db2e5f39f5b8a1d301e045ed83513ae0355ea
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2024-11-23 18:10:02 +0800

    lazy

--- x/contract/keeper/keeper_gc.go
+++ x/contract/keeper/keeper_gc.go
@@ -15,9 +15,41 @@ import (
 func (k Keeper) GarbageCollect(goCtx context.Context) error {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	err := k.GarbageCollectOrder(ctx)
+	// This should be faster for releasing escrow amount
+	err := k.GarbageCollectLazyContract(ctx)
 	if err != nil {
 		return err
+	}
+
+	err = k.GarbageCollectOrder(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (k Keeper) GarbageCollectLazyContract(ctx sdk.Context) error {
+	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
+	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.SortedLazyContractKeyPrefix))
+	iterator := storetypes.KVStorePrefixIterator(store, []byte{})
+
+	defer iterator.Close()
+
+	// Because expiry is contained in the key, expiry will be iterated in ascending order
+	for ; iterator.Valid(); iterator.Next() {
+		var val types.SortedLazyContract
+		k.cdc.MustUnmarshal(iterator.Value(), &val)
+
+		if uint64(ctx.BlockTime().UnixMilli()) < val.Expiry {
+			break
+		}
+
+		// TODO: release escrow amount
+		// Penalized if pending amount is positive
+
+		store.Delete(iterator.Key())
+		k.RemoveLazyContract(ctx, val.Index)
 	}
 
 	return nil
@@ -41,29 +73,6 @@ func (k Keeper) GarbageCollectOrder(ctx sdk.Context) error {
 
 		store.Delete(iterator.Key())
 		k.RemoveOrder(ctx, val.Id)
-	}
-
-	return nil
-}
-
-func (k Keeper) GarbageCollectLazyContract(ctx sdk.Context) error {
-	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
-	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.SortedLazyContractKeyPrefix))
-	iterator := storetypes.KVStorePrefixIterator(store, []byte{})
-
-	defer iterator.Close()
-
-	// Because expiry is contained in the key, expiry will be iterated in ascending order
-	for ; iterator.Valid(); iterator.Next() {
-		var val types.SortedLazyContract
-		k.cdc.MustUnmarshal(iterator.Value(), &val)
-
-		if uint64(ctx.BlockTime().UnixMilli()) < val.Expiry {
-			break
-		}
-
-		store.Delete(iterator.Key())
-		k.RemoveLazyContract(ctx, val.Index)
 	}
 
 	return nil
