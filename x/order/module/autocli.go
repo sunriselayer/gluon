b233567a8310d57e275e6c08b38ae2ec1c5ae15b
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2024-12-01 15:43:27 +0800

    autocli

--- x/order/module/autocli.go
+++ x/order/module/autocli.go
@@ -18,26 +18,15 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					Short:     "Shows the parameters of the module",
 				},
 				{
-					RpcMethod: "OrderAll",
+					RpcMethod: "Orders",
 					Use:       "list-order",
 					Short:     "List all order",
 				},
 				{
 					RpcMethod:      "Order",
-					Use:            "show-order [id]",
+					Use:            "show-order [hash]",
 					Short:          "Shows a order",
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "index"}},
-				},
-				{
-					RpcMethod: "SortedOrderAll",
-					Use:       "list-sorted-order",
-					Short:     "List all sorted_order",
-				},
-				{
-					RpcMethod:      "SortedOrder",
-					Use:            "show-sorted-order [id]",
-					Short:          "Shows a sorted_order",
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "index"}},
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "hash"}},
 				},
 				// this line is used by ignite scaffolding # autocli/query
 			},
@@ -49,42 +38,6 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 				{
 					RpcMethod: "UpdateParams",
 					Skip:      true, // skipped because authority gated
-				},
-				{
-					RpcMethod:      "CreateOrder",
-					Use:            "create-order [index]",
-					Short:          "Create a new order",
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "index"}},
-				},
-				{
-					RpcMethod:      "UpdateOrder",
-					Use:            "update-order [index]",
-					Short:          "Update order",
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "index"}},
-				},
-				{
-					RpcMethod:      "DeleteOrder",
-					Use:            "delete-order [index]",
-					Short:          "Delete order",
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "index"}},
-				},
-				{
-					RpcMethod:      "CreateSortedOrder",
-					Use:            "create-sorted-order [index]",
-					Short:          "Create a new sorted_order",
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "index"}},
-				},
-				{
-					RpcMethod:      "UpdateSortedOrder",
-					Use:            "update-sorted-order [index]",
-					Short:          "Update sorted_order",
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "index"}},
-				},
-				{
-					RpcMethod:      "DeleteSortedOrder",
-					Use:            "delete-sorted-order [index]",
-					Short:          "Delete sorted_order",
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "index"}},
 				},
 				// this line is used by ignite scaffolding # autocli/tx
 			},
