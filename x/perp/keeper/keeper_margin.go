9e54b804e954efebd85aad514223817873ba84ae
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2024-12-03 15:29:57 +0800

    comment

--- x/perp/keeper/keeper_margin.go
+++ x/perp/keeper/keeper_margin.go
@@ -8,6 +8,8 @@ import (
 	"gluon/x/perp/types"
 )
 
+// AddCrossMargin adds cross margin asset to the store
+// BankKeeper.SendCoins is not included
 func (k Keeper) AddCrossMargin(ctx context.Context, owner string, asset sdk.Coin) sdk.Coins {
 	crossMargin, found := k.GetCrossMargin(ctx, owner)
 
