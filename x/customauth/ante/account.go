59a35bbbacd5114e57d5789868abc182bc581020
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-12-19 17:41:03 +0900

    feat: add SetAccountDecorator

---
+++ x/customauth/ante/account.go
@@ -0,0 +1,42 @@
+package ante
+
+import (
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
+)
+
+// SetAccountDecorator creates a new BaseAccount, if the signer is not exist on chain.
+// This process is necessary when an account without balance sends tx with operator's signature.
+// On Gluon, it is possible to send tx with the user's sequence and operator's signature without any fee.
+type SetAccountDecorator struct {
+	accountKeeper AccountKeeper
+}
+
+func NewSetAccountDecorator(ak AccountKeeper) SetAccountDecorator {
+	return SetAccountDecorator{
+		accountKeeper: ak,
+	}
+}
+
+func (dfd SetAccountDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
+	sigTx, ok := tx.(authsigning.SigVerifiableTx)
+	if !ok {
+		return ctx, errorsmod.Wrap(sdkerrors.ErrTxDecode, "invalid transaction type")
+	}
+
+	signers, err := sigTx.GetSigners()
+	if err != nil {
+		return sdk.Context{}, err
+	}
+
+	for _, signer := range signers {
+		accExists := dfd.accountKeeper.HasAccount(ctx, signer)
+		if !accExists {
+			dfd.accountKeeper.SetAccount(ctx, dfd.accountKeeper.NewAccountWithAddress(ctx, signer))
+		}
+	}
+
+	return next(ctx, tx, simulate)
+}
