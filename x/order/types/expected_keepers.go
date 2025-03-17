acd8d69c578e4ef48d6d592bd7a8a1d2f94987a0
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2025-03-17 11:03:06 +0800

    expected keeper

--- x/order/types/expected_keepers.go
+++ x/order/types/expected_keepers.go
@@ -1,1 +1,30 @@
 package types
+
+import (
+	"context"
+
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	customauthtypes "gluon/x/customauth/types"
+	"gluon/x/customauth/types/pairing"
+)
+
+// AccountKeeper defines the expected interface for the Account module.
+type AccountKeeper interface {
+	GetAccount(context.Context, sdk.AccAddress) sdk.AccountI // only used for simulation
+	// Methods imported from account should be defined here
+	GetModuleAddress(moduleName string) sdk.AccAddress
+}
+
+// BankKeeper defines the expected interface for the Bank module.
+type BankKeeper interface {
+	SpendableCoins(context.Context, sdk.AccAddress) sdk.Coins
+	// Methods imported from bank should be defined here
+}
+
+type CustomAuthKeeper interface {
+	GetPairing(ctx context.Context, owner string, index string) (val customauthtypes.Pairing, found bool)
+	UnpackPairingPubKey(pairing customauthtypes.Pairing) (cryptotypes.PubKey, error)
+	GetPairingPubKeyInternal(ctx context.Context, user sdk.AccAddress, pairingIndex string) (pairing.PubKeyInternal, error)
+}
